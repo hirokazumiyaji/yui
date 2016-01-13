6775a6747dd08f3d19cd7f6e06374e1f0f5259f3
Hirokazu Miyaji <hirokazu.miyaji@gmail.com>
2016-01-13 23:25:43 +0900

    es6

--- scripts/nurupo.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-module.exports = function exports(robot) {
-  robot.hear(/ぬるぽ/i, function (res) {
-    res.send("ガッ");
-  });
-};
--- src/nurupo.js
+++
@@ -1,5 +0,0 @@
-exports = (robot => {
-  robot.hear(/ぬるぽ/i, res => {
-    res.send("ガッ")
-  })
-})
